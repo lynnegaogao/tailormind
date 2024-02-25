b6e2ad462dd7b3871e2460615c03e7e5631f2760
King <1415052695@qq.com>
2024-02-25 15:57:44 +0800

    Add FilePreview component and file asset for displaying PDF files

---
+++ src/components/FilePreview.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <vue-pdf :src="pdfPath" />
+    </div>
+</template>
+
+<script>
+import { pdf } from 'vue-pdf';
+
+export default {
+    components: {
+        VuePdf: pdf,
+    },
+    data() {
+        return {
+            pdfPath: require('@/assets/cs229-notes12.pdf'), // Provide the path to your local PDF file
+        };
+    },
+};
+</script>
